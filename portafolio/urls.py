658720ba006311ff8c4aa05cdd63140c0146bbd2
lostgrammer <carlos_cmvp@hotmail.com>
2022-12-10 18:36:39 -0500

    agregando una nueva app y ruta

--- portafolio/urls.py
+++ portafolio/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#importando
+from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #nueva ruta
+    path('',views.index),
 ]
